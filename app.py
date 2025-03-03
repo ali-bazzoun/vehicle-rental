38fe444144973cdf1ec024104fe6e2d6a2d706c1
ali-bazzoun <abazzoun540@gmail.com>
2025-03-03 02:40:46 +0200

    Refactor to use show_vehicle_info function for polymorphic vehicle display

--- app.py
+++ app.py
@@ -1,14 +1,20 @@
+from vehicle import Vehicle
 from car import Car
 from motorcycle import Motorcycle
+
+
+def show_vehicle_info(vehicle: Vehicle):
+    vehicle.display_info()
 
 def main():
 
     car = Car("Toyota", "Corolla", 2019, 50.0, 5)
     motorcycle = Motorcycle("Honda", "CBR", 2020, 40.0, 250)
 
-    
-    car.display_info()
-    motorcycle.display_info()
+    vehicles = [car, motorcycle]
+
+    for vehicle in vehicles:
+        show_vehicle_info(vehicle)
 
 if __name__ == "__main__":
     main()
